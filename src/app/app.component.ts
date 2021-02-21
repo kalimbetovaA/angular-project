0923ca3829a0da821713018725c7fcee28bc8804
kalimbetovaA <kalimbetova.00@gmail.com>
2021-02-22 00:12:25 +0600

    Assignment-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
-    datax = [
+    dataArr = [
     {
       title: "Лучшее от Zapis.kz",
-      salons: new Array<string>('Hikari Lazer', 'Barbershop Klaus', 'Lilu', 'SASS AFD Plaza')
+      salons: [{
+        image: "https://zapis.kz/data/pics/salon/pic-11164.jpg?v=1",
+        salon: "Hikari Lazer"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-971.jpg?v=1",
+        salon: "Barbershop Klaus"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-38244.jpg?v=0",
+        salon: "Lilu"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-26851.jpg?v=1",
+        salon: "SASS AFD Plaza"
+      }
+      ]
     },
     {
       title: "Салоны с кэшбеком",
-      salons: new Array<string>('QUEEN BEE', '«Бороду не брей» на Акан Cеры', 'Vilena', 'Sandi Beksirga на Наурызбай батыра')
+      salons: [ {
+        image: "https://zapis.kz/data/pics/salon/pic-33493.jpg?v=1",
+        salon: "QUEEN BEE"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-18246.jpg?v=0",
+        salon: "«Бороду не брей» на Акан Cеры"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-7656.jpg?v=0",
+        salon: "Vilena"
+      },
+      {
+        image: "https://zapis.kz/data/pics/salon/pic-5061.jpg?v=1",
+        salon: "Sandi Beksirga на Наурызбай батыра"
+      }
+      ]
     }
     
     ];
